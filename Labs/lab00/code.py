eb47615d8b44589a12c22ea12a3e50d37d79b82b
fengweidiezhen <80031372+fengweidiezhen@users.noreply.github.com>
2023-09-24 17:35:57 +0800

    Add files via upload

---
+++ Labs/lab00/code.py
@@ -0,0 +1,20 @@
+def get_airspeed_velocity_of(unladen_swallow):
+  if unladen_swallow.type == "african":
+    return # redacted
+  elif unladen_swallow.type == "european":
+    return # redacted
+
+# pretend there's code here...
+
+def fizzbuzz(num):
+  if num % 15 == 0:
+    print(f"{num}: fizzbuzz")
+  elif num % 3 == 0: # edit this line
+    print(f"{num}: fizz")
+  elif num % 5 == 0: # edit this line
+    print(f"{num}: buzz")
+
+for i in range(1, 20):
+  fizzbuzz(i)
+
+# pretend there's code here...
